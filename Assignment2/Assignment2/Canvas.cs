30b4ecdb39370a77cd391ca6cd96f9e2b6565176
pinion7 <pinion7rl@gmail.com>
2021-06-14 12:27:17 +0900

    Assignment2 try15#2

--- Assignment2/Assignment2/Canvas.cs
+++ Assignment2/Assignment2/Canvas.cs
@@ -174,105 +174,6 @@ namespace Assignment2
             return result;
         }
 
-        //        public static bool IsShape(char[,] canvas, EShape shape)
-        //        {
-
-        //            bool bResult = true;
-
-        //            char[,] rectangleArray = Draw((uint)canvas.GetLength(1) - 4, (uint)canvas.GetLength(0) - 4, EShape.Rectangle);
-        //            char[,] isoscelesTriangleArray = Draw((uint)canvas.GetLength(1) - 4, (uint)canvas.GetLength(0) - 4, EShape.IsoscelesTriangle);
-        //            char[,] isoscelesRightTriangleArray = Draw((uint)canvas.GetLength(1) - 4, (uint)canvas.GetLength(0) - 4, EShape.IsoscelesRightTriangle);
-        //            char[,] circleArray = Draw((uint)canvas.GetLength(1) - 4, (uint)canvas.GetLength(0) - 4, EShape.Circle);
-        //            //char[,] newArray = Draw((uint)canvas.GetLength(1) - 4, (uint)canvas.GetLength(0) - 4, shape);
-
-        //            //if (canvas.GetLength(0) - 4 > 0 && canvas.GetLength(1) - 4 > 0 && newArray.GetLength(0) != 0 && newArray.GetLength(1) != 0)
-        //            //{
-
-        //            //    for (int i = 0; i < canvas.GetLength(0); ++i)
-        //            //    {
-        //            //        for (int j = 0; j < canvas.GetLength(1); ++j)
-        //            //        {
-        //            //            if (canvas[i, j] == newArray[i, j])
-        //            //            {
-        //            //                bResult = true;
-        //            //            }
-
-        //            //            else
-        //            //            {
-        //            //                bResult = false;
-        //            //                break;
-        //            //            }
-        //            //        }
-        //            //    }
-
-        //            //}
-
-        //            if (canvas.GetLength(0) == 0 || canvas.GetLength(1) == 0 || canvas.GetLength(0) - 4 <= 0 || canvas.GetLength(1) - 4 <= 0)
-        //            {
-        //                return false;
-        //            }
-
-        //            else if (canvas.GetLength(0) == rectangleArray.GetLength(0) || canvas.GetLength(0) == isoscelesRightTriangleArray.GetLength(0) || canvas.GetLength(0) == isoscelesTriangleArray.GetLength(0) || canvas.GetLength(0) == circleArray.GetLength(0))
-        //            {
-
-        //                if (shape == EShape.Rectangle)
-        //                {
-        //                    for (int i = 0; i < canvas.GetLength(0); ++i)
-        //                    {
-        //                        for (int j = 0; j < canvas.GetLength(1); ++j)
-        //                        {
-        //                            if (canvas[i, j] != rectangleArray[i, j])
-        //                            {
-        //                                bResult = false;
-        //                            }
-        //                        }
-        //                    }
-        //                }
-
-        //                else if (shape == EShape.IsoscelesRightTriangle)
-        //                {
-        //                    for (int i = 0; i < canvas.GetLength(0); ++i)
-        //                    {
-        //                        for (int j = 0; j < canvas.GetLength(1); ++j)
-        //                        {
-        //                            if (canvas[i, j] != isoscelesRightTriangleArray[i, j])
-        //                            {
-        //                                bResult = false;
-        //                            }
-        //                        }
-        //                    }
-        //                }
-
-        //                else if (shape == EShape.IsoscelesTriangle)
-        //                {
-        //                    for (int i = 0; i < canvas.GetLength(0); ++i)
-        //                    {
-        //                        for (int j = 0; j < canvas.GetLength(1); ++j)
-        //                        {
-        //                            if (canvas[i, j] != isoscelesTriangleArray[i, j])
-        //                            {
-        //                                bResult = false;
-        //                            }
-        //                        }
-        //                    }
-        //                }
-
-        //                else if (shape == EShape.Circle)
-        //                {
-        //                    for (int i = 0; i < canvas.GetLength(0); ++i)
-        //                    {
-        //                        for (int j = 0; j < canvas.GetLength(1); ++j)
-        //                        {
-        //                            if (canvas[i, j] != circleArray[i, j])
-        //                            {
-        //                                bResult = false;
-        //                            }
-        //                        }
-        //                    }
-        //                }
-        //            }
-        //            return bResult;
-        //        }
         public static bool IsShape(char[,] canvas, EShape shape)
         {
             bool bResult = false;
@@ -304,20 +205,20 @@ namespace Assignment2
                     bResult = false;
                 }
 
-                else if ((canvas.GetLength(0) != canvas.GetLength(1) || canvas[2, 2] != '*' || canvas[2, canvas.GetLength(1) - 3] != ' ') && shape == EShape.IsoscelesRightTriangle)
-                {
-                    bResult = false;
-                }
+                //else if ((canvas.GetLength(0) != canvas.GetLength(1) || canvas[2, 2] != '*' || canvas[2, canvas.GetLength(1) - 3] != ' ') && shape == EShape.IsoscelesRightTriangle)
+                //{
+                //    bResult = false;
+                //}
 
                 else if (canvas.GetLength(0) == canvas.GetLength(1) && canvas[2, 2] == '*' && canvas[2, canvas.GetLength(1) - 3] == ' ' && shape == EShape.IsoscelesRightTriangle)
                 {
                     bResult = true;
                 }
 
-                else if ((canvas[2, 2] != '*' || canvas[2, canvas.GetLength(1) - 3] != '*') && shape == EShape.Rectangle)
-                {
-                    bResult = false;
-                }
+                //else if ((canvas[2, 2] != '*' || canvas[2, canvas.GetLength(1) - 3] != '*') && shape == EShape.Rectangle)
+                //{
+                //    bResult = false;
+                //}
 
                 else if (canvas[2, 2] == '*' && canvas[2, canvas.GetLength(1) - 3] == '*' && shape == EShape.Rectangle)
                 {
@@ -329,102 +230,3 @@ namespace Assignment2
         }
     }
 }
-
-
-//if (canvas.GetLength(0) - 4 != 0 && canvas.GetLength(1) - 4 != 0 && canvas.GetLength(0) != 0 && canvas.GetLength(1) != 0)
-//{
-
-//    if (canvas.GetLength(0) - 4 == 1 && canvas.GetLength(1) - 4 == 1)
-//    {
-//        if (shape == EShape.Circle && CircleEqual(canvas))
-//        {
-//            bResult = true;
-//        }
-
-//        else if (shape == EShape.IsoscelesRightTriangle && RightTriangleEqual(canvas))
-//        {
-//            bResult = true;
-//        }
-
-//        else if (shape == EShape.Rectangle && RectangleEqual(canvas))
-//        {
-//            bResult = true;
-//        }
-
-//        else
-//        {
-//            bResult = false;
-//        }
-//    }
-
-//    //else if (canvas.GetLength(0) == canvas.GetLength(1) && canvas.GetLength(1) % 2 != 0 && shape == EShape.Circle)
-//    //{
-//    //    bResult = true;
-//    //}
-
-//    else if ((canvas.GetLength(0) != canvas.GetLength(1) || canvas.GetLength(1) % 2 == 0) && shape == EShape.Circle)
-//    {
-//        bResult = false;
-//    }
-
-//    //else if (canvas.GetLength(1) - 4 == (canvas.GetLength(0) - 4) * 2 - 1 && shape == EShape.IsoscelesTriangle)
-//    //{
-//    //    bResult = true;
-//    //}
-
-//    else if (canvas.GetLength(1) - 4 != (canvas.GetLength(0) - 4) * 2 - 1 && shape == EShape.IsoscelesTriangle)
-//    {
-//        bResult = false;
-//    }
-
-//    else if ((canvas.GetLength(0) != canvas.GetLength(1) || canvas[2, 2] != '*' || canvas[2, canvas.GetLength(1) - 3] != ' ') && shape == EShape.IsoscelesRightTriangle)
-//    {
-//        bResult = false;
-//    }
-
-//    //else if (canvas.GetLength(0) == canvas.GetLength(1) && canvas[2, 2] == '*' && canvas[2, canvas.GetLength(1) - 3] == ' ' && shape == EShape.IsoscelesRightTriangle)
-//    //{
-//    //    bResult = true;
-//    //}
-
-//    else if ((canvas[2, 2] != '*' || canvas[2, canvas.GetLength(1) - 3] != '*') && shape == EShape.Rectangle)
-//    {
-//        bResult = false;
-//    }
-
-//    //else if (canvas[2, 2] == '*' && canvas[2, canvas.GetLength(1) - 3] == '*' && shape == EShape.Rectangle)
-//    //{
-//    //    bResult = true;
-//    //}
-
-//    else
-//    {
-//        bResult = true;
-//    }
-//}
-
-
-
-
-//if (canvas.GetLength(0) - 4 == 1 && canvas.GetLength(1) - 4 == 1)
-//{
-//    if (canvas[2, 2] == '*' && canvas[2, canvas.GetLength(1) - 3] == '*' && shape == EShape.Rectangle)
-//    {
-//        bResult = true;
-//    }
-
-//    else if (canvas.GetLength(1) - 4 == (canvas.GetLength(0) - 4) * 2 - 1 && shape == EShape.IsoscelesTriangle)
-//    {
-//        bResult = true;
-//    }
-
-//    else if (canvas.GetLength(0) == canvas.GetLength(1) && canvas.GetLength(1) % 2 != 0 && shape == EShape.Circle)
-//    {
-//        bResult = true;
-//    }
-
-//    else if (canvas[2, 2] == '*' && shape == EShape.IsoscelesRightTriangle)
-//    {
-//        bResult = true;
-//    }
-//}
